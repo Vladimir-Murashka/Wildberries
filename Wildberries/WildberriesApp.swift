774023e7ae45e33627e0f13a889b26b632e23afc
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2024-06-30 16:55:51 +0300

    Initial Commit

---
+++ Wildberries/WildberriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildberriesApp.swift
+//  Wildberries
+//
+//  Created by Vladimir Murashka on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WildberriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
